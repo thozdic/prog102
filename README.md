5af81fd8ec104beaefc6bc9c853a8f27baa6906d
thozdic <thozdic@ucsb.edu>
2025-01-21 08:11:59 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Intro to Marine Data Science
+
+## PROG102: Functions
+
+### Before class
+
+See [details about this module](https://marinecs-100b.github.io/tracks/prog/prog102.html) on the course website. Guided notes are in 'PROG102 Guided Notes.pdf'. Take notes directly on the guided notes, either by annotating the PDF or printing them out.
+
+### In class
+
+Edit 'prog102.R' with your answers. [Submission instructions](https://marinecs-100b.github.io/module-submission.html) are on the course website.
---
+++ prog102.R
@@ -0,0 +1,33 @@
+library(marinecs100b)
+
+# Extracting data ---------------------------------------------------------
+
+# How did you extract the temperature and exposure from the hottest day?
+# Copy-paste the code here.
+
+# If you want to change the site and date, what variables would need to change?
+
+# What would you pick for the temperature extraction function name and parameter
+# names?
+
+
+# Writing extraction functions --------------------------------------------
+
+# Write your temperature extraction function here
+
+# Write your exposure extraction function here
+
+# Write your datetime extraction function here
+
+# Visualize Nuka Pass temperatures on 2018-07-01
+
+# How does the visualization code above compare to what you wrote in PROG101?
+
+
+# Functions within functions (optional) -----------------------------------
+
+# How would you parameterize the visualization code into its own function?
+# Specifically, what would you call that function and its parameters?
+
+# Write your wrapper function for plot_kefj(). Call it to compare the
+# visualizations for Nuka Pass on July 1, 2018 versus January 1, 2018.
